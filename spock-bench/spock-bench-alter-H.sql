8c4ab6fdbe1ea37403178ca533c67ad293dff38b
Jan Wieck <jan@wi3ck.info>
2023-02-09 16:58:38 -0500

    Revert "Partition spock-bench history table by bid."
    This reverts commit 92da1e339d297cf63e17525daa4cfb193887bde9.
    
    Row filtering should actually be done in the subscription
    via the row_filter=<EXPR> parameter to replication_set_add_table().
    It can be done via partitioning, but that is a different,
    optional setup.

--- spock-bench/spock-bench-alter-H.sql
+++ spock-bench/spock-bench-alter-H.sql
@@ -5,14 +5,6 @@
 --       This can be accomplished by running this SQL script via
 --       pgbench ... -D branch=N -f spock-bench-alter.sql ...
 -- ----------------------------------------------------------------------
-DROP TABLE pgbench_history;
-
--- CREATE TABLE pgbench_history (hid bigserial, tid integer, bid integer, aid integer, delta integer, mtime timestamp, filler char(22), PRIMARY KEY (hid));
-
-CREATE TABLE pgbench_history (hid bigserial, tid integer, bid integer, aid integer, delta integer, mtime timestamp, filler char(22), PRIMARY KEY (bid, hid)) PARTITION BY RANGE (bid);
-
-CREATE TABLE pgbench_history_node1 PARTITION OF pgbench_history FOR VALUES FROM (1) TO (2);
-CREATE TABLE pgbench_history_node2 PARTITION OF pgbench_history FOR VALUES FROM (2) TO (3);
-CREATE TABLE pgbench_history_node3 PARTITION OF pgbench_history FOR VALUES FROM (3) TO (4);
-
+ALTER TABLE pgbench_history ADD COLUMN hid bigserial;
+ALTER TABLE pgbench_history ADD PRIMARY KEY (hid);
 ALTER SEQUENCE pgbench_history_hid_seq START :branch RESTART :branch INCREMENT 100;
