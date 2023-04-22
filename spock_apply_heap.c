6b63ea568af5a6553cd25acd647efe5743ad26c4
denis lussier <denis@lussier.io>
2023-04-22 18:39:45 +0000

    more (c) fixes

--- spock_apply_heap.c
+++ spock_apply_heap.c
@@ -1,9 +1,11 @@
 /*-------------------------------------------------------------------------
  *
  * spock_apply_heap.c
- * 		spock apply functions using heap api
+ *             spock apply functions using heap api
  *
- * Copyright (c) 2015, PostgreSQL Global Development Group
+ * Copyright (c) 2022-2023, pgEdge, Inc.
+ * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, The Regents of the University of California
  *
  * IDENTIFICATION
  *		  spock_apply_heap.c
