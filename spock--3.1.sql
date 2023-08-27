80e76a99d692731c1914d730412a91b37ef92345
Jan Wieck <jan@wi3ck.info>
2023-08-27 19:44:16 -0400

    Add roles and permissions to all spock objects
    To operate with no superuser we create roles
    spock_admin and spock_monitor that will then be
    granted during a database setup. The extension
    itself will be owned by the superuser cluster
    owner and all spock.*() functions are created
    SECURITY DEFINER.

--- spock--3.1.sql
+++ spock--3.1.sql
@@ -335,14 +335,18 @@ GRANT SELECT ON TABLE spock.queue TO spock_admin;
 CREATE FUNCTION spock.replicate_ddl(command text, replication_sets text[] DEFAULT '{ddl_sql}')
 RETURNS boolean STRICT VOLATILE SECURITY DEFINER LANGUAGE c AS 'MODULE_PATHNAME', 'spock_replicate_ddl_command';
 GRANT EXECUTE ON FUNCTION spock.replicate_ddl(text, text[]) TO spock_admin;
+<<<<<<< HEAD
 CREATE FUNCTION spock.replicate_ddl(command text[], replication_sets text[] DEFAULT '{ddl_sql}')
 RETURNS SETOF boolean STRICT VOLATILE SECURITY DEFINER LANGUAGE sql AS
     'SELECT spock.replicate_ddl(cmd, $2) FROM (SELECT unnest(command) cmd)';
 GRANT EXECUTE ON FUNCTION spock.replicate_ddl(text[], text[]) TO spock_admin;
 
+=======
+>>>>>>> 95e46f3 (Add roles and permissions to all spock objects)
 CREATE FUNCTION spock.replicate_ddl(command text[], replication_sets text[] DEFAULT '{ddl_sql}')
-RETURNS SETOF boolean STRICT VOLATILE LANGUAGE sql AS
+RETURNS SETOF boolean STRICT VOLATILE SECURITY DEFINER LANGUAGE sql AS
     'SELECT spock.replicate_ddl(cmd, $2) FROM (SELECT unnest(command) cmd)';
+GRANT EXECUTE ON FUNCTION spock.replicate_ddl(text[], text[]) TO spock_admin;
 
 CREATE FUNCTION spock.queue_truncate()
 RETURNS trigger SECURITY DEFINER LANGUAGE c AS 'MODULE_PATHNAME', 'spock_queue_truncate';
@@ -526,5 +530,3 @@ $$
 	SET session_replication_role TO 'replica';
 $$;
 GRANT EXECUTE ON FUNCTION spock.set_session_replication_role_replica TO spock_admin;
-=======
->>>>>>> 413f07c (add md5 agg function for checksumming rows &/or groups of rows &/or tables)
