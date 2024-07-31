99b231b6b37bb628c0842fc0a81721da516d6d95
Jan Wieck <jan@wi3ck.info>
2024-07-30 20:00:36 -0400

    Add conflict resolution status tables for ACE
    ACE, as well as any other future tool that deals with conflict
    resolution, needs tables where it can track the status of a
    conflict. This is both on the transaction level as well as on
    the individual row level conflict within the transaction.

--- spock--4.0.0--4.0.1.sql
+++ spock--4.0.0--4.0.1.sql
@@ -3,3 +3,25 @@
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION spock UPDATE TO '4.0.1'" to load this file. \quit
 
+CREATE TABLE spock.exception_status (
+	remote_origin oid NOT NULL,
+	remote_commit_ts timestamptz NOT NULL,
+	remote_xid bigint NOT NULL,
+	status text NOT NULL,
+	resolved_at timestamptz,
+	resolution_details jsonb,
+	PRIMARY KEY(remote_origin, remote_commit_ts)
+) WITH (user_catalog_table=true);
+
+CREATE TABLE spock.exception_status_detail (
+	remote_origin oid NOT NULL,
+    remote_commit_ts timestamptz NOT NULL,
+	command_counter integer NOT NULL,
+	remote_xid bigint NOT NULL,
+	status text NOT NULL,
+	resolved_at timestamptz,
+	resolution_details jsonb,
+	PRIMARY KEY(remote_origin, remote_commit_ts, command_counter),
+	FOREIGN KEY(remote_origin, remote_commit_ts)
+		REFERENCES spock.exception_status
+) WITH (user_catalog_table=true);
--- spock--4.0.1.sql
+++ spock--4.0.1.sql
@@ -67,6 +67,29 @@ CREATE TABLE spock.exception_log (
 	PRIMARY KEY(remote_origin, remote_commit_ts, command_counter)
 ) WITH (user_catalog_table=true);
 
+CREATE TABLE spock.exception_status (
+	remote_origin oid NOT NULL,
+	remote_commit_ts timestamptz NOT NULL,
+	remote_xid bigint NOT NULL,
+	status text NOT NULL,
+	resolved_at timestamptz,
+	resolution_details jsonb,
+	PRIMARY KEY(remote_origin, remote_commit_ts)
+) WITH (user_catalog_table=true);
+
+CREATE TABLE spock.exception_status_detail (
+	remote_origin oid NOT NULL,
+    remote_commit_ts timestamptz NOT NULL,
+	command_counter integer NOT NULL,
+	remote_xid bigint NOT NULL,
+	status text NOT NULL,
+	resolved_at timestamptz,
+	resolution_details jsonb,
+	PRIMARY KEY(remote_origin, remote_commit_ts, command_counter),
+	FOREIGN KEY(remote_origin, remote_commit_ts)
+		REFERENCES spock.exception_status
+) WITH (user_catalog_table=true);
+
 CREATE FUNCTION spock.node_create(node_name name, dsn text,
     location text DEFAULT NULL, country text DEFAULT NULL,
     info jsonb DEFAULT NULL)
