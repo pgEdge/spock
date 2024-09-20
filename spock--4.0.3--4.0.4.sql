583b81b4941e06bf0d6a66955bdcc3dfa5b68f16
Jan Wieck <jan@wi3ck.info>
2024-09-20 10:01:08 -0400

    Changing version to 4.1.0
    This changes the Spock version of current development (main)
    to 4.1.0 in order to distinguish from REL4_0_STABLE.

---
+++ spock--4.0.3--4.0.4.sql
@@ -0,0 +1,5 @@
+
+/* spock--4.0.2--4.0.3.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '4.0.3'" to load this file. \quit
