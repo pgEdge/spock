8533ba62512ac6476ffb474ee60349abaa0ebe99
Jan Wieck <jan@wi3ck.info>
2024-12-18 15:14:10 -0500

    Stamping 4.0.8

--- spock.h
+++ spock.h
@@ -26,8 +26,8 @@
 
 #include "spock_compat.h"
 
-#define SPOCK_VERSION "4.0.7"
-#define SPOCK_VERSION_NUM 40007
+#define SPOCK_VERSION "4.0.8"
+#define SPOCK_VERSION_NUM 40008
 
 #define SPOCK_MIN_PROTO_VERSION_NUM 2
 #define SPOCK_MAX_PROTO_VERSION_NUM 2
