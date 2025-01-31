781a6e550c693ad5b195858f932bf78542bdb101
Asif Rehman <asifr@pgedge.com>
2025-01-31 16:17:49 +0500

    Adjusted upgrade scripts for 4.0.x -> 4.1.0

--- spock--4.0.9--4.1.0.sql
+++ spock--4.0.9--4.1.0.sql
@@ -8,9 +8,10 @@ CREATE TABLE spock.progress (
 	node_id oid NOT NULL,
 	remote_node_id oid NOT NULL,
 	remote_commit_ts timestamptz NOT NULL,
+	remote_lsn pg_lsn NOT NULL,
 	PRIMARY KEY(node_id, remote_node_id)
 ) WITH (fillfactor=50);
 
-INSERT INTO spock.progress (node_id, remote_node_id, remote_commit_ts)
-	SELECT sub_target, sub_origin, 'epoch'::timestamptz
+INSERT INTO spock.progress (node_id, remote_node_id, remote_commit_ts, remote_lsn)
+	SELECT sub_target, sub_origin, 'epoch'::timestamptz, '0/0'
 	FROM spock.subscription;
