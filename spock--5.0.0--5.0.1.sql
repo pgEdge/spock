56334e402defedab3697d2708d418a614dd48e38
Mason Sharp <mason.sharp@pgedge.com>
2025-07-28 13:18:36 -0700

    Revert change to 5.0.0.
    Put change in spock--5.0.0--5.0.1.sql

--- spock--5.0.0--5.0.1.sql
+++ spock--5.0.0--5.0.1.sql
@@ -2,3 +2,10 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION spock UPDATE TO '5.0.1'" to load this file. \quit
+
+
+CREATE OR REPLACE FUNCTION spock.replicate_ddl(command text,
+                                    replication_sets text[] DEFAULT '{ddl_sql}',
+                                    search_path text DEFAULT current_setting('search_path'),
+                                    role text DEFAULT CURRENT_USER)
+RETURNS boolean STRICT VOLATILE LANGUAGE c AS 'MODULE_PATHNAME', 'spock_replicate_ddl_command';
