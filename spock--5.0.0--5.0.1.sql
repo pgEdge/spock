7c76f8692be0984dcd89837f9e55eb63a4afd7fa
Mason Sharp <mason.sharp@pgedge.com>
2025-07-29 15:28:49 -0700

    Change version to 5.0.1

---
+++ spock--5.0.0--5.0.1.sql
@@ -0,0 +1,4 @@
+/* spock--5.0.0--5.0.1.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.1'" to load this file. \quit
