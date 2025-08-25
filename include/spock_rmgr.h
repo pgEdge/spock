4a76a86b1208a2315e4f304dd9c379fba0c71c8b
Asif Rehman <asifr@pgedge.com>
2025-08-25 18:44:43 +0500

    Added struct for ProgressInfo and SubTransaction commit ts.

--- include/spock_rmgr.h
+++ include/spock_rmgr.h
@@ -12,6 +12,24 @@
 #define SPOCK_RMGR_PROGRESS_INFO			0x10
 #define SPOCK_RMGR_SUBTRANS_COMMIT_TS       0x20
 
+typedef struct ProgressInfoEntry
+{
+	Oid         dbid;
+	Oid         node_id;
+    Oid         remote_node_id;
+	TimestampTz remote_commit_ts;
+    XLogRecPtr  remote_lsn;
+    XLogRecPtr  remote_insert_lsn;
+	TimestampTz last_updated_ts;
+    bool        updated_by_decode;
+} ProgressInfoEntry;
+
+typedef struct SubTransactionCommitTsEntry
+{
+	TransactionId	xid;
+	TimestampTz		time;
+	RepOriginId		nodeid;
+} SubTransactionCommitTsEntry;
 
 extern void spock_rmgr_init(void);
 extern void spock_rmgr_desc(StringInfo buf, XLogReaderState *record);
@@ -19,5 +37,6 @@ extern const char *spock_rmgr_identify(uint8 info);
 extern void spock_rmgr_redo(XLogReaderState *record);
 extern void spock_rmgr_startup(void);
 extern void spock_rmgr_cleanup(void);
+
 
 #endif /* SPOCK_RMGR_H */
