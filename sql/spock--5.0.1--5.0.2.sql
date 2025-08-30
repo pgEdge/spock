08ba630afb9c446a612481873485818621b374a5
Mason Sharp <mason.sharp@pgedge.com>
2025-08-29 17:56:16 -0700

    Upgrade paths from 4.0.11 to 5.0.2

---
+++ sql/spock--5.0.1--5.0.2.sql
@@ -0,0 +1,5 @@
+
+/* spock--5.0.1--5.0.2.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.2'" to load this file. \quit
