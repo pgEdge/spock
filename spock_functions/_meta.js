c6b128acd54e8a29546b0fecb0b5dcefe91fbe92
Susan Douglas <susan@pgedge.local>
2025-09-26 08:09:35 -0400

    Removed extra folder that contained spock_functions - the real folder is nested in the docs folder

--- spock_functions/_meta.js
+++
@@ -1,8 +0,0 @@
-export default {
-    index: 'Spock Functions',
-    functions: 'Function Reference',
-    node_mgmt: 'Node Mgmt Functions',
-    repset_mgmt: 'Replication Set Mgmt Functions',
-    sub_mgmt: 'Subscription Mgmt Functions',
-    gen_mgmt: 'Cluster Mgmt Functions',
-};
