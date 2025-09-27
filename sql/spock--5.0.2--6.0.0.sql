cad812a839f9b3ff72738e20ef300f6f423a192b
Mason Sharp <mason.sharp@pgedge.com>
2025-09-26 17:23:27 -0700

    Add upgrade path for 5.0.4
    Include missing files, modified from cherry-pick
    
    (cherry picked from commit 3c932d27cc29a7bddd10240956d7af86ec34bccc)

--- sql/spock--5.0.2--6.0.0.sql
+++
@@ -1,4 +0,0 @@
-/* spock--5.0.2--6.0.0.devel.sql */
-
--- complain if script is sourced in psql, rather than via ALTER EXTENSION
-\echo Use "ALTER EXTENSION spock UPDATE TO '6.0.0-devel'" to load this file. \quit
---
+++ sql/spock--5.0.3--5.0.4.sql
@@ -0,0 +1,5 @@
+
+/* spock--5.0.3--5.0.4.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.4'" to load this file. \quit
