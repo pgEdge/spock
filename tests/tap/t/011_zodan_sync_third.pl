7e199ba257f994679fd04a0f67f708453b6280c1
Andrei V. Lepikhov <lepihov@gmail.com>
2025-11-04 17:04:57 +0100

    Plain Z0DAN SYNC test for the 2+1 node configuration.
    A node is added under high load of 'blind' UPDATE transactions.
    These transactions designed to update separate parts of the table on
    different nodes to avoid any conflict issues during the test.

---
+++ tests/tap/t/011_zodan_sync_third.pl
@@ -0,0 +1,185 @@
+use strict;
+use warnings;
+use Test::More tests => 20;
+use IPC::Run;
+use lib '.';
+use lib 't';
+use SpockTest qw(create_cluster destroy_cluster system_or_bail get_test_config cross_wire psql_or_bail scalar_query);
+
+# =============================================================================
+# Test: Add third node (N3) to the configuration of highly loaded (N1 and N2)
+# by non-intersecting DMLs.
+# =============================================================================
+# This test follows the sequence:
+# 1. Create nodes N1 and N2
+# 2. Init pgbench database
+# 3. CHECK: database replicated and we see a 'zero' lag
+# 4. Load N1 and N2 with a custom non-intersecting UPDATE load
+# 5. Call add_node() on N3
+# 6. Check that pgbench load still exists after the end of the Z0DAN protocol
+# 7. Wait for the end of the test and final data sync.
+# 8. Check consistency of the data on each node.
+# 9. Clean up
+
+create_cluster(3, 'Create initial 2-node Spock test cluster');
+
+
+my ($ret1, $ret2, $ret3);
+
+# Get cluster configuration
+my $config = get_test_config();
+my $node_count = $config->{node_count};
+my $node_ports = $config->{node_ports};
+my $host = $config->{host};
+my $dbname = $config->{db_name};
+my $db_user = $config->{db_user};
+my $db_password = $config->{db_password};
+my $pg_bin = $config->{pg_bin};
+
+cross_wire(2, ['n1', 'n2'], 'Cross-wire nodes N1 and N2');
+
+note "Install the helper functions and do other preparatory stuff";
+my $helper_sql = '../../samples/Z0DAN/wait_subscription.sql';
+my $zodan_sql = '../../samples/Z0DAN/zodan.sql';
+psql_or_bail(1, "\\i $helper_sql");
+psql_or_bail(2, "\\i $helper_sql");
+psql_or_bail(3, "\\i $zodan_sql");
+psql_or_bail(3, "CREATE EXTENSION dblink");
+psql_or_bail(3, "SELECT spock.node_drop('n3')");
+
+psql_or_bail(1, "ALTER SYSTEM SET log_min_messages TO LOG");
+psql_or_bail(1, "SELECT pg_reload_conf()");
+psql_or_bail(2, "ALTER SYSTEM SET log_min_messages TO LOG");
+psql_or_bail(2, "SELECT pg_reload_conf()");
+
+note "Initialize pgbench database and wait for initial sync on N1 and N2 ...";
+system_or_bail "$pg_bin/pgbench", '-i', '-s', 1, '-h', $host,
+							'-p', $node_ports->[0], '-U', $db_user, $dbname;
+# Wait until tables and data will be sent to N2
+psql_or_bail(1, 'SELECT spock.wait_slot_confirm_lsn(NULL, NULL)');
+# Test 1: after the end of replication process we should be able to see a 'zero'
+# lag between the nodes.
+my $lag = scalar_query(2, "SELECT * FROM wait_subscription(remote_node_name := 'n1',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+
+ok($lag  <= 0, "Initial replication has been successful");
+
+# Create non-intersecting load for nodes N1 and N2.
+# Test duration should be enough to cover all the Z0DAN stages. We will kill
+# pgbench immediately after the N3 is attached.
+my $load1 = '../../samples/Z0DAN/n1.pgb';
+my $load2 = '../../samples/Z0DAN/n2.pgb';
+my $pgbench_stdout1='';
+my $pgbench_stderr1='';
+my $pgbench_stdout2='';
+my $pgbench_stderr2='';
+my $pgbench_handle1 = IPC::Run::start(
+    [ "$pg_bin/pgbench", '-n', '-f', $load1, '-T', 80, '-j', 3, '-c', 3,
+	'-h', $host, '-p', $node_ports->[0], '-U', $db_user, $dbname],
+	'>', \$pgbench_stdout1, '2>', \$pgbench_stderr1);
+my $pgbench_handle2 = IPC::Run::start(
+    [ "$pg_bin/pgbench", '-n', '-f', $load2, '-T', 80, '-j', 3, '-c', 3,
+	'-h', $host, '-p', $node_ports->[1], '-U', $db_user, $dbname],
+	'>', \$pgbench_stdout2, '2>', \$pgbench_stderr2);
+$pgbench_handle1->pump();
+$pgbench_handle2->pump();
+
+# Warming up ...
+note "warming up pgbench for 5s";
+sleep(5);
+note "done warmup";
+
+note "Add N3 into highly loaded configuration of N1 and N2 ...";
+psql_or_bail(3,
+	"CALL spock.add_node(src_node_name := 'n1',
+						 src_dsn := 'host=$host dbname=$dbname port=$node_ports->[0] user=$db_user',
+						 new_node_name := 'n3',
+						 new_node_dsn := 'host=$host dbname=$dbname port=$node_ports->[2] user=$db_user',
+						 verb := false);");
+
+# Ensure that pgbench load lasts longer than the Z0DAN protocol.
+my $pid = $pgbench_handle1->{KIDS}[0]{PID};
+my $alive = kill 0, $pid;
+ok($alive eq 1, "pgbench load to N1 still exists");
+$pid = $pgbench_handle2->{KIDS}[0]{PID};
+$alive = kill 0, $pid;
+ok($alive eq 1, "pgbench load to N2 still exists");
+
+note "Kill pgbench process to reduce test time";
+$pgbench_handle1->pump();
+$pgbench_handle2->pump();
+$pgbench_handle1->kill_kill;
+$pgbench_handle2->kill_kill;
+
+note "Check if pgbench finalised correctly";
+$pgbench_handle1->finish;
+$pgbench_handle2->finish;
+note "##### output of pgbench #####";
+note $pgbench_stdout1;
+note $pgbench_stdout2;
+note "##### end of output #####";
+
+psql_or_bail(1, 'SELECT spock.wait_slot_confirm_lsn(NULL, NULL)');
+psql_or_bail(2, 'SELECT spock.wait_slot_confirm_lsn(NULL, NULL)');
+
+note "Wait until the end of replication ..";
+$lag = scalar_query(1, "SELECT * FROM wait_subscription(remote_node_name := 'n2',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N2 => N1 has been finished successfully");
+$lag = scalar_query(2, "SELECT * FROM wait_subscription(remote_node_name := 'n1',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N1 => N2 has been finished successfully");
+$lag = scalar_query(3, "SELECT * FROM wait_subscription(remote_node_name := 'n1',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N1 => N3 has been finished successfully");
+$lag = scalar_query(3, "SELECT * FROM wait_subscription(remote_node_name := 'n2',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N2 => N3 has been finished successfully");
+
+note "Check the data consistency.";
+$ret1 = scalar_query(1, "SELECT sum(abalance), sum(aid), count(*) FROM pgbench_accounts");
+note "The N1's pgbench_accounts aggregates: $ret1";
+$ret2 = scalar_query(2, "SELECT sum(abalance), sum(aid), count(*) FROM pgbench_accounts");
+note "The N2's pgbench_accounts aggregates: $ret2";
+$ret3 = scalar_query(3, "SELECT sum(abalance), sum(aid), count(*) FROM pgbench_accounts");
+note "The N3's pgbench_accounts aggregates: $ret3";
+
+ok($ret1 eq $ret3, "Equality of the data on N1 and N3 is confirmed");
+ok($ret2 eq $ret3, "Equality of the data on N2 and N3 is confirmed");
+
+# Before we finish this test and destroy the cluster, we need to ensure that
+# the nodes are not stuck in some work. N1 and N2 have done their job and are
+# ready to switch off. However, after receiving a large amount of WAL during
+# catch-up, N3 may be decoding WAL to determine a proper LSN for N3->N1 and
+# N3->N2 replication.
+# Being in this process, the walsender doesn't send anything valuable except
+# a 'keepalive' message. Hence, we can't clearly detect the end of the process.
+# So, nudge it, employing the sync_event machinery.
+psql_or_bail(3, "SELECT spock.sync_event()");
+note "Wait for the end of N3->N1, N3->N2 decoding process that means the actual start of LR";
+psql_or_bail(3, 'SELECT spock.wait_slot_confirm_lsn(NULL, NULL)');
+
+# Nothing sensitive should be awaited here, just to be sure.
+$lag = scalar_query(1, "SELECT * FROM wait_subscription(remote_node_name := 'n3',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N2 => N1 has been finished successfully");
+$lag = scalar_query(2, "SELECT * FROM wait_subscription(remote_node_name := 'n3',
+														   report_it := true,
+														   timeout := '10 minutes',
+														   delay := 1.)");
+ok($lag  <= 0, "Replication N1 => N2 has been finished successfully");
+
+# Cleanup will be handled by SpockTest.pm END block
+# No need for done_testing() when using a test plan
