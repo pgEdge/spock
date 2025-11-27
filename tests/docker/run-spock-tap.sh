c799a70a60ab0df3a40913a297ade70d460ceff3
Andrei V. Lepikhov <lepihov@gmail.com>
2025-11-27 11:55:14 +0100

    Separate the long-lasting Z0DAN sync TAP test.
    Designed to be highly competitive, this test consumes more CPU, disk, and
    memory resources than usual. To let people pass basic checks on their ongoing
    commits faster, turn off this test in the schedule and separate it as a GitHub
    action.
    
    Also, it appears that we copied Spock into the base image. Hence, the 'step-1'
    Docker image might use oldish versions of the patches.

---
+++ tests/docker/run-spock-tap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+###
+### Run selected TAP tests iteratively
+###
+
+
+export PG_CONFIG=/home/pgedge/pgedge/pg${PGVER}/bin/pg_config
+export PATH=/home/pgedge/pgedge/pg${PGVER}/bin:$PATH
+export LD_LIBRARY_PATH=/home/pgedge/pgedge/pg${PGVER}/lib/:$LD_LIBRARY_PATH
+export PROVE_TESTS=$proven_tests
+
+# PGVER should be previously set in the environment
+if [ -z "${PGVER}" ]
+then
+	echo "The PGVER environment variable must be set before running this command"
+	exit 1
+fi
+
+proven_tests="$1"
+iterations="$2"
+if [[ -z "$proven_tests" || -z "$iterations" ]]; then
+    echo "Command-line parameters are set incorrectly"
+    exit 1
+fi
+
+cd /home/pgedge/spock/
+
+status=0
+for i in $(seq 1 $iterations); do
+    echo "Iteration $i: running make check..."
+    env PROVE_TESTS="$proven_tests" make check_prove 1>out.txt 2>err.txt
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo "make check failed with status $status on iteration $i"
+        break
+    fi
+done
+
+if [ $status -ne 0 ]
+then
+	echo "Errors in regression checks"
+	exit 1
+fi
