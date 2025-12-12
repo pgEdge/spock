9a11965baca49b6ec12395e5db17af86340d9278
Andrei V. Lepikhov <lepihov@gmail.com>
2025-12-12 10:44:51 +0100

    Change the progress arrangement logic when syncing a subscription.
    The COPY procedure may take a long time. So, when loading the progress state
    after the COPY on an actively processing transactions Spock 3+ configuration,
    there's a risk that more transactions from ‘other’ nodes have arrived and
    been applied on the ‘donor’ node.
    
    So, if someone wants to continue LR from the ‘other’ nodes without conflicting
    with data copied from the ‘donor’ node, we need to calculate the precise state
    of replication progress consistent with the (snapshot, LSN) returned by
    the CREATE_REPLICATION_SLOT syncing operation.
    
    Taking the progress state right before creating a slot narrows the window
    during which ‘other’ transactions may arrive and be committed by apply workers.
    For sure, it is not an ultimate solution, but a step in the right direction.
    The following steps are:
    
    1. Write a test that shows the problem may evolve if something extends the gap
    in the subscription sync procedure (probably using injection points?).
    2. Write a function that scans the gap and updates the progress state.
    
    NOTE:
    This commit doesn’t change the table sync code. It is not clear whether we need
    to arrange the entire LR progress on an N3 node to the current state of
    a ‘donor’ node N1, since someone decided to resync a single table. So, let it
    behave as usual.

--- src/spock_sync.c
+++ src/spock_sync.c
@@ -397,7 +397,7 @@ ensure_replication_origin(char *slot_name)
 
 
 static List *
-adjust_progress_info(PGconn *origin_conn, PGconn *target_conn)
+adjust_progress_info(PGconn *origin_conn)
 {
 	const char *originQuery =
 		"SELECT * FROM spock.progress "
@@ -911,7 +911,7 @@ copy_tables_data(SpockSubscription *sub, const char *origin_dsn,
 		CHECK_FOR_INTERRUPTS();
 	}
 
-	progress_entries_list = adjust_progress_info(origin_conn, target_conn);
+	progress_entries_list = adjust_progress_info(origin_conn);
 
 	/* Finish the transactions and disconnect. */
 	finish_copy_origin_tx(origin_conn);
@@ -939,7 +939,6 @@ copy_replication_sets_data(SpockSubscription *sub, const char *origin_dsn,
 	PGconn	   *target_conn;
 	List	   *tables;
 	ListCell   *lc;
-	List	   *progress_entries_list = NIL;
 
 	/* Connect to origin node. */
 	origin_conn = spock_connect(origin_dsn, sub->name, "copy");
@@ -1001,16 +1000,9 @@ copy_replication_sets_data(SpockSubscription *sub, const char *origin_dsn,
 		CHECK_FOR_INTERRUPTS();
 	}
 
-	progress_entries_list = adjust_progress_info(origin_conn, target_conn);
-
 	/* Finish the transactions and disconnect. */
 	finish_copy_origin_tx(origin_conn);
 	finish_copy_target_tx(target_conn);
-
-	/*
-	 * Update replication progress. We must do it after commit of the COPY.
-	 */
-	spock_group_progress_update_list(progress_entries_list);
 
 	return tables;
 }
@@ -1145,6 +1137,7 @@ spock_sync_subscription(SpockSubscription *sub)
 		PGconn	   *origin_conn_repl;
 		char	   *snapshot;
 		bool		use_failover_slot;
+		List	   *progress_entries_list = NIL;
 
 		elog(INFO, "initializing subscriber %s", sub->name);
 
@@ -1160,6 +1153,7 @@ spock_sync_subscription(SpockSubscription *sub)
 		origin_conn_repl = spock_connect_replica(sub->origin_if->dsn,
 													 sub->name, "snap");
 
+		progress_entries_list = adjust_progress_info(origin_conn);
 		snapshot = ensure_replication_slot_snapshot(origin_conn,
 													origin_conn_repl,
 													sub->slot_name,
@@ -1278,13 +1272,18 @@ spock_sync_subscription(SpockSubscription *sub)
 			}
 			PG_END_ENSURE_ERROR_CLEANUP_SUFFIX(spock_sync_tmpfile_cleanup_cb,
 										CStringGetDatum(tmpfile), _suf);
-			spock_sync_tmpfile_cleanup_cb(0,
-											  CStringGetDatum(tmpfile));
+			spock_sync_tmpfile_cleanup_cb(0, CStringGetDatum(tmpfile));
 		}
 		PG_END_ENSURE_ERROR_CLEANUP(spock_sync_worker_cleanup_error_cb,
 									PointerGetDatum(sub));
 
 		PQfinish(origin_conn_repl);
+
+		/*
+		 * Update replication progress. We must do it after commit of the all
+		 * syncing operations.
+		 */
+		spock_group_progress_update_list(progress_entries_list);
 
 		status = SYNC_STATUS_CATCHUP;
 		StartTransactionCommand();
