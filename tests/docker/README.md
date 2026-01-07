c39dd63f4aba12f229f42679b69ffd28f6bfceed
Andrei V. Lepikhov <lepihov@gmail.com>
2026-01-07 17:01:14 +0100

    2

--- tests/docker/README.md
+++ tests/docker/README.md
@@ -1,1 +1,62 @@
-# see [pgedge-docker repo](https://github.com/pgedge/pgedge-docker) repo for container guidance
+# Docker infrastructure for the Spock testing
+
+This document describes how the Docker infrastructure is organized, how to use it for
+testing locally and as a part of a GitHub Actions workflow.
+
+## Structure
+
+The Spock Docker environment provides one `stable` image cached by the URL ghcr.io/pgedge/base-test-image:latest and a `floating` one, created on the fly during the tests.
+
+The `stable` image is created according to the `Dockerfile-base.el9` file and should be built manually by the `cache-base-image.yml` workflow. It contains a Rocky Linux image with a complete set of libraries necessary for the Postgres build and passing regression and TAP tests. This image does not define an entry point.
+
+The `floating` image is defined by the `Dockerfile-step-1.el9` file and is intended to be used in tests. This image defines a default `tests/docker/entrypoint.sh` entry point.
+
+### Main parameters of the `stable` image
+* Base OS: Rocky Linux 9
+* Default user: `pgedge` (with sudo privileges, password: `asdf`)
+* Current working directory: `/home/pgedge`
+* SSH keys: id_ed25519 and corresponding id_ed25519.pub in the folder `~/.ssh` are provided (authorized for passwordless SSH as pgedge user)
+* Includes complete PostgreSQL build dependencies (LLVM, ICU, SSL, etc.)
+* Testing tools: Perl Test::More, SSH server/client configured
+
+### Main parameters of the `floating` image
+* Based on the `stable` image
+* Build arguments:
+  * `PGVER` - PostgreSQL major version to build (e.g., 15, 16, 17, 18)
+  * `MAKE_JOBS=4` - number of parallel make jobs (default: 4)
+* Default user: `pgedge`
+* Current working directory: `/home/pgedge`
+* PostgreSQL installation:
+  * Source: Cloned from official PostgreSQL GitHub repository (latest stable tag for specified major version)
+  * Location: `/home/pgedge/postgres` (source code)
+  * Install prefix: `/home/pgedge/pgedge/pg${PGVER}`
+  * Patches: Spock-specific patches from `/home/pgedge/spock/patches/${PGVER}/` are applied before compilation
+  * Build configuration: Debug mode enabled (`-g -O0`), with TAP tests, LLVM, assertions, dtrace, and full extension support
+* pgEdge CLI tool:
+  * Installed via `https://pgedge-download.s3.amazonaws.com/REPO/install.py`
+  * Location: `/home/pgedge/pgedge/pgedge`
+* Spock extension:
+  * Source: Copied from build context to `/home/pgedge/spock`
+  * Compiled and installed against the patched PostgreSQL
+* Environment variables:
+  * `PGVER` - PostgreSQL major version
+  * `PATH` - includes `/home/pgedge/pgedge/pg${PGVER}/bin`
+  * `LD_LIBRARY_PATH` - includes `/home/pgedge/pgedge/pg${PGVER}/lib`
+  * `PG_CONFIG` - points to `/home/pgedge/pgedge/pg${PGVER}/bin/pg_config`
+* Test scripts: All `tests/docker/*.sh` scripts available in `/home/pgedge/`
+* Default entrypoint: `/home/pgedge/entrypoint.sh`
+
+## Usage
+
+Each test type - regression, TAP tests, spockbech or any new one must employ the `Dockerfile-step-1` docker image in tests. An instance initialisation and launch commands must be implemented in an entry point. If test builds custom environment (like regression or TAP tests) it have to define separate docker entry point.
+
+For example, `tests/docker/run-spock-regress.sh` is an entry point for regression tests; `tests/tap/run_tests.sh` is an entry point for TAP tests. In opposite, spockbench creates three-node infrastructure based on the `docker/tests/docker-compose.yml` script and default entry point.
+
+## Notes
+
+* If you need an extra package, add it to the `Dockerfile-base.el9` file, updating the library list. Afterwards, re-run the workflow to rebuild the image.
+* **OpenSSL compatibility**: Rocky Linux 9 ships with OpenSSL 3.5.1, while pgEdge's PostgreSQL may include OpenSSL 3.4.0. To avoid library conflicts, Python commands during build and runtime should temporarily unset `LD_LIBRARY_PATH` using `env -u LD_LIBRARY_PATH python3 ...`
+
+## References
+
+* See the [pgedge-docker repo](https://github.com/pgedge/pgedge-docker) for container guidance
--- tests/docker/entrypoint.sh
+++ tests/docker/entrypoint.sh
@@ -16,94 +16,38 @@ function wait_for_pg()
   done
 }
 
-. /home/pgedge/.bashrc
-
 echo "==========Installing Spockbench=========="
 cd ~/spockbench
 sudo python3 setup.py install
 
 cd ~/pgedge
+
+# Paths to the Postgres binaries and CLI
+#export PATH=/home/pgedge/pgedge/pg${PGVER}/bin:$PATH
+#export LD_LIBRARY_PATH=/home/pgedge/pgedge/pg${PGVER}/lib:/usr/lib64:/lib64
+
+# Initialize the database cluster
+# We avoid 'pgedge setup' command to not download meaningless repo. Hence,
+# initialise repository in our own.
+#mkdir data
+
+
+echo "PGHOME=`pwd`/pg17" > pg17.env
+echo "PGDATA=`pwd`/data/pg17" >> pg17.env
+echo "PGUSER=admin" >> pg17.env
+echo "PGDATABASE=demo" >> pg17.env
+echo "PGPORT=5432" >> pg17.env
+
+pgedge init pg17
+
+mkdir data/pg${PGVER}
+~/pgedge/pg${PGVER}/bin/initdb -D data/pg${PGVER} --encoding=UTF8 --locale=C
+
 sed -i '/log_min_messages/s/^#//g' data/pg$PGVER/postgresql.conf
 sed -i -e '/log_min_messages =/ s/= .*/= debug1/' data/pg$PGVER/postgresql.conf
-./pgedge restart
+
+pgedge start pg17
 
 wait_for_pg
 
-psql -h /tmp -U $DBUSER -d $DBNAME -c "drop extension spock;"
-psql -h /tmp -U $DBUSER -d $DBNAME -c "drop schema public cascade;"
-psql -h /tmp -U $DBUSER -d $DBNAME -c "create schema public;"
-psql -h /tmp -U $DBUSER -d $DBNAME -c "create extension spock;"
-
-./pgedge restart
-
-wait_for_pg
-
-echo "==========Assert Spock version is the latest=========="
-expected_line=$(grep '#define SPOCK_VERSION' /home/pgedge/spock/spock.h)
-expected_version=$(echo "$expected_line" | grep -oP '"\K[0-9]+\.[0-9]+\.[0-9]+')
-expected_major=${expected_version%%.*}
-actual_version=$(psql -U $DBUSER -d $DBNAME -X -t -A -c "select spock.spock_version()")
-actual_major=${actual_version%%.*}
-
-if (( actual_major >= expected_major )); then
-  echo " Actual major version ($actual_major) >= expected ($expected_major)"
-else
-  echo " Actual major version ($actual_major) is not what we expected ($expected_major)"
-  exit 1
-fi
-
-
-echo "==========Creating tables and repsets=========="
-./pgedge spock node-create $HOSTNAME "host=$HOSTNAME user=pgedge dbname=$DBNAME" $DBNAME
-./pgedge spock repset-create demo_replication_set $DBNAME
-
-IFS=',' read -r -a peer_names <<< "$PEER_NAMES"
-
-for PEER_HOSTNAME in "${peer_names[@]}";
-do
-  while :
-    do
-      mapfile -t node_array < <(psql -A -t $DBNAME -h $PEER_HOSTNAME -c "SELECT node_name FROM spock.node;")
-      for element in "${node_array[@]}";
-      do
-        if [[ "$element" == "$PEER_HOSTNAME" ]]; then
-            break 2
-        fi
-      done
-      sleep 1
-      echo "Waiting for $PEER_HOSTNAME..."
-    done
-done
-
-# TODO: Re-introduce parallel slots at a later point when the apply worker restarts are handled correctly
-# and transactions are not skipped on restart in parallel mode
-./pgedge spock sub-create sub_${peer_names[0]}$HOSTNAME   "host=${peer_names[0]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[0]}$HOSTNAME"_1 "host=${peer_names[0]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[0]}$HOSTNAME"_2 "host=${peer_names[0]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[0]}$HOSTNAME"_3 "host=${peer_names[0]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[0]}$HOSTNAME"_4 "host=${peer_names[0]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-
-./pgedge spock sub-create sub_${peer_names[1]}$HOSTNAME   "host=${peer_names[1]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[1]}$HOSTNAME"_1 "host=${peer_names[1]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[1]}$HOSTNAME"_2 "host=${peer_names[1]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[1]}$HOSTNAME"_3 "host=${peer_names[1]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-#./pgedge spock sub-create "sub_${peer_names[1]}$HOSTNAME"_4 "host=${peer_names[1]} port=5432 user=pgedge dbname=$DBNAME" $DBNAME
-
-psql -U $DBUSER -h /tmp -d $DBNAME -c "create table t1 (id serial primary key, data int8);"
-psql -U $DBUSER -h /tmp -d $DBNAME -c "create table t2 (id serial primary key, data int8);"
-psql -U $DBUSER -h /tmp -d $DBNAME -c "alter table t1 alter column data set (log_old_value=true, delta_apply_function=spock.delta_apply);"
-
-./pgedge spock sub-add-repset sub_${peer_names[0]}$HOSTNAME demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[0]}$HOSTNAME"_1 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[0]}$HOSTNAME"_2 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[0]}$HOSTNAME"_3 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[0]}$HOSTNAME"_4 demo_replication_set $DBNAME
-
-./pgedge spock sub-add-repset sub_${peer_names[1]}$HOSTNAME demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[1]}$HOSTNAME"_1 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[1]}$HOSTNAME"_2 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[1]}$HOSTNAME"_3 demo_replication_set $DBNAME
-#./pgedge spock sub-add-repset "sub_${peer_names[1]}$HOSTNAME"_4 demo_replication_set $DBNAME
-
-
-cd /home/pgedge && ./run-tests.sh $peer_names
+psql -h /tmp -U $DBUSER -d $DBNAME -c "SELECT version()"
