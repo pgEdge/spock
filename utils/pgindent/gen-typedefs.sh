1f20e817cb09a50f0d63fd6505954f737ab4d179
Andrei V. Lepikhov <lepihov@gmail.com>
2026-01-07 10:54:42 +0100

    Fix pgindent script to prevent typedef duplicates
    The gen-typedefs.sh script accumulated duplicates on each run because
    it only appended to typedefs.list. Fixed by:
    - Uncommenting curl to fetch fresh PostgreSQL typedefs (overwrites file)
    - Adding sort/uniq to remove any internal duplicates
    - Cleaning up typedefs.list
    
    Sorting allow us to stabilise the content in case a struct or enum
    has been displaced.
    This ensures repeated script runs produce consistent, deduplicated output.

--- utils/pgindent/gen-typedefs.sh
+++ utils/pgindent/gen-typedefs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-#curl https://buildfarm.postgresql.org/cgi-bin/typedefs.pl > typedefs.list
+# Fetch PostgreSQL core typedefs from buildfarm
+curl https://buildfarm.postgresql.org/cgi-bin/typedefs.pl > typedefs.list
 
+# Extract Spock-specific typedefs from source code and append them
 grep -nri "typedef struct" ../../ --include="*.c" --include="*.h" | grep -v "{" | awk '{print $3}' >> typedefs.list
 grep -nri "typedef enum" ../../ --include="*.c" --include="*.h" | grep -v "{" | awk '{print $3}' >> typedefs.list
 grep -nri "typedef union" ../../ --include="*.c" --include="*.h" | grep -v "{" | awk '{print $3}' >> typedefs.list
+
+# Remove duplicates and sort
+sort typedefs.list | uniq > typedefs.list.tmp && mv typedefs.list.tmp typedefs.list
 
 find ../../ -type f \( -name "*.c" -o -name "*.h" \) -print0 \
   | xargs -0 pgindent --typedefs typedefs.list
