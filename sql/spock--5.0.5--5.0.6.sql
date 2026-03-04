6b78b53d066d03657258882d448092c92f58ef7b
Mason Sharp <mason.sharp@pgedge.com>
2026-03-04 13:09:33 -0800

    Add message to spock--5.0.5--5.0.6.sql

--- sql/spock--5.0.5--5.0.6.sql
+++ sql/spock--5.0.5--5.0.6.sql
@@ -1,2 +1,7 @@
+/* spock--5.0.5--5.0.6.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.6'" to load this file. \quit
+
 ALTER TABLE spock.subscription
     ADD COLUMN sub_created_at timestamptz;
