ca07b9cbf00d1f76c8b3f0ef4ac68c346b35a7d5
Mason Sharp <mason.sharp@pgedge.com>
2026-03-11 16:29:14 -0700

    Use LSN_FORMAT_ARGS

--- src/spock_conflict.c
+++ src/spock_conflict.c
@@ -488,8 +488,7 @@ spock_report_conflict(SpockConflictType conflict_type,
 							   remotetup.data,
 							   replorigin_session_origin,
 							   timestamptz_to_str(replorigin_session_origin_timestamp),
-							   (uint32) (replorigin_session_origin_lsn << 32),
-							   (uint32) replorigin_session_origin_lsn)));
+							   LSN_FORMAT_ARGS(replorigin_session_origin_lsn))));
 			break;
 		case SPOCK_CT_UPDATE_MISSING:
 			ereport(spock_conflict_log_level,
@@ -502,8 +501,7 @@ spock_report_conflict(SpockConflictType conflict_type,
 							   remotetup.data,
 							   replorigin_session_origin,
 							   timestamptz_to_str(replorigin_session_origin_timestamp),
-							   (uint32) (replorigin_session_origin_lsn << 32),
-							   (uint32) replorigin_session_origin_lsn)));
+							   LSN_FORMAT_ARGS(replorigin_session_origin_lsn))));
 			break;
 		case SPOCK_CT_DELETE_MISSING:
 			ereport(spock_conflict_log_level,
@@ -515,8 +513,7 @@ spock_report_conflict(SpockConflictType conflict_type,
 					 errdetail("tuple for remote delete in xact origin=%u,timestamp=%s,commit_lsn=%X/%X",
 							   replorigin_session_origin,
 							   timestamptz_to_str(replorigin_session_origin_timestamp),
-							   (uint32) (replorigin_session_origin_lsn << 32),
-							   (uint32) replorigin_session_origin_lsn)));
+							   LSN_FORMAT_ARGS(replorigin_session_origin_lsn))));
 			break;
 		case SPOCK_CT_DELETE_ORIGIN_DIFFERS:
 			ereport(spock_conflict_log_level,
@@ -531,8 +528,7 @@ spock_report_conflict(SpockConflictType conflict_type,
 								local_tup_ts_str,
 								replorigin_session_origin,
 								timestamptz_to_str(replorigin_session_origin_timestamp),
-								(uint32) (replorigin_session_origin_lsn << 32),
-								(uint32) replorigin_session_origin_lsn)));
+								LSN_FORMAT_ARGS(replorigin_session_origin_lsn))));
 			break;
 		case SPOCK_CT_DELETE_LATE:
 			ereport(spock_conflict_log_level,
@@ -544,8 +540,7 @@ spock_report_conflict(SpockConflictType conflict_type,
 					 errdetail("remote delete in xact origin=%u,timestamp=%s,commit_lsn=%X/%X",
 							   replorigin_session_origin,
 							   timestamptz_to_str(replorigin_session_origin_timestamp),
-							   (uint32) (replorigin_session_origin_lsn << 32),
-							   (uint32) replorigin_session_origin_lsn)));
+							   LSN_FORMAT_ARGS(replorigin_session_origin_lsn))));
 			break;
 	}
 }
