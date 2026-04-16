3141dc951729c858931e6ddffa0470dcd4c3d7b7
Mason Sharp <mason.sharp@pgedge.com>
2026-04-16 11:42:45 -0700

    Bump to version 5.0.7
    In preparation of upcoming .sql file changes

---
+++ sql/spock--5.0.6--5.0.7.sql
@@ -0,0 +1,5 @@
+
+/* spock--5.0.6--5.0.7.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.7'" to load this file. \quit
