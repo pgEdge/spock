e27d9f7f8daeb3d57543321241966077ffa1ed66
Mason Sharp <mason.sharp@pgedge.com>
2026-04-19 14:26:03 -0700

    Move DDL to new SQL file

--- sql/spock--5.0.6--5.0.7.sql
+++ sql/spock--5.0.6--5.0.7.sql
@@ -3,3 +3,12 @@
 
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION spock UPDATE TO '5.0.7'" to load this file. \quit
+
+CREATE FUNCTION spock.pause_apply_workers()
+RETURNS void VOLATILE LANGUAGE c AS 'MODULE_PATHNAME', 'spock_pause_apply_workers';
+
+CREATE FUNCTION spock.resume_apply_workers()
+RETURNS void VOLATILE LANGUAGE c AS 'MODULE_PATHNAME', 'spock_resume_apply_workers';
+
+REVOKE EXECUTE ON FUNCTION spock.pause_apply_workers() FROM PUBLIC;
+REVOKE EXECUTE ON FUNCTION spock.resume_apply_workers() FROM PUBLIC;
