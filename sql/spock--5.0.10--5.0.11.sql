2fa38e945048b69ee938a7913ef7ca6b9eabccea
Mason Sharp <mason.sharp@pgedge.com>
2026-07-21 20:02:00 -0700

    Prepare v5.0.11

---
+++ sql/spock--5.0.10--5.0.11.sql
@@ -0,0 +1,6 @@
+/* spock--5.0.10--5.0.11.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION spock UPDATE TO '5.0.11'" to load this file. \quit
+
+-- No schema changes in 5.0.11.
